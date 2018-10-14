6bd24b78804fd065d21955ddc31edd98b2eff75d
Jeffry Yanuar <je_freedom2029indo@yahoo.co.id>
2018-10-14 10:43:13 +0800

    Ignore ApiKey

--- CookingRecipe/CookingRecipe/CookingRecipe/Services/RecipeService.cs
+++ CookingRecipe/CookingRecipe/CookingRecipe/Services/RecipeService.cs
@@ -8,11 +8,12 @@ namespace CookingRecipe.Services
     public class RecipeService : HttpService, IRecipeService
     {
         private static readonly string BASE_URI = "https://food2fork.com/api";
-        private static readonly string API_KEY = ""; // TODO: Add API Key
+        
 
         public async Task<RecipeList> GetRecipes(string query)
         {
-            Uri url = new Uri(BASE_URI + $"/search?key={API_KEY}&q={query}");
+            Uri url = new Uri(
+                BASE_URI + $"/search?key={CookingRecipe.Configs.ApiKey.API_KEY}&q={query}");  // TODO: Add API Key
             var response = await SendRequestAsync<RecipeList>(url, HttpMethod.Get);
 
             return response;
